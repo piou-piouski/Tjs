f70888bca06887af392c54616d4283630b345a1b
wierz <wierzbicki.y@hotmail.fr>
2023-05-22 10:59:54 +0100

    install yarn

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import logo from './logo.svg';
+import { MemeSVGViewer } from 'orsys-tjs-meme';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      AGUR
+      <MemeSVGViewer />
+    </div>
+  );
+}
+
+export default App;
